fef6fe0bcd34de39989f8c1ca9514d8421b79fca
Vitor1317 <vitoralvesmoraes111@gmail.com>
2023-08-08 23:07:04 -0300

    make:about

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const nav = document.querySelector(".navigation");
-const body = document.querySelector("body");
 
 function openMenu() {
-    body.classList.add("overlay");
     nav.classList.add("open");
 }
 
 function closeMenu() {
-    body.classList.remove("overlay");
     nav.classList.remove("open");
 }
