c2106e5ab573afb17db781905a3a16c58b80e242
Vitor1317 <vitoralvesmoraes111@gmail.com>
2023-08-17 20:41:43 -0300

    make:website

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector(".navigation");
+const pAccodion = document.querySelectorAll('.accordionHidden')
+const iconRotate = document.querySelectorAll('.ph-plus')
 
 function openMenu() {
     nav.classList.add("open");
@@ -6,4 +8,19 @@ function openMenu() {
 
 function closeMenu() {
     nav.classList.remove("open");
+}
+
+function accordion(element, icon) {
+    for(let i =0; i < pAccodion.length; i++) {
+        if(!pAccodion[i].classList.contains('accordionHidden') && pAccodion[i] !== element) {
+            pAccodion[i].classList.add('accordionHidden')
+        }
+
+        if(iconRotate[i].classList.contains('iconRotate') && iconRotate[i] !== icon) {
+            iconRotate[i].classList.remove('iconRotate')
+        }
+    }
+
+    icon.classList.toggle('iconRotate')
+    element.classList.toggle('accordionHidden')
 }
